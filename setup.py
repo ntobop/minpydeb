f95d9acbb52d9d2725add83f2eae5439865dd074
Norihiro Tobo <ntobo@pepabo.com>
2023-02-24 17:40:04 +0900

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='minpydeb',
+    version='1.0.0',
+    package_dir={"": "src"},
+    py_modules=['minpydeb'],
+    include_package_data=True,
+    install_requires=['']
+)
+
