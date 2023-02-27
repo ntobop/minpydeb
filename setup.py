ddfd843de8dfb8264d55fa1bf97a59b2137460d1
ntobo <122763915+ntobop@users.noreply.github.com>
2023-02-27 10:49:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='minpydeb',
     version='1.0.0',
-    package_dir={"": "src"},
+    package_dir={"": "src/minpydeb"},
     py_modules=['minpydeb'],
     include_package_data=True,
     install_requires=['']
